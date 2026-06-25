109b46c903ec863ab76b11910b884b38eec85709
Davy-programator <davyrewel007@gmail.com>
2026-06-25 07:22:16 -0400

    codigo final

--- Trabalho_FrontEnd3_Davy_e_Gabriel_1025B/script.js
+++ Trabalho_FrontEnd3_Davy_e_Gabriel_1025B/script.js
@@ -7,9 +7,7 @@ const historicoTabela = document.getElementById("historico");
 
 let historico =
     JSON.parse(localStorage.getItem("historicoRick")) || [];
-
 renderizarHistorico();
-
 botao.addEventListener("click", buscarPersonagem);
 
 async function buscarPersonagem() {
@@ -21,62 +19,43 @@ async function buscarPersonagem() {
     }
 
     try {
-
         erro.textContent = "";
-
         const resposta =
             await fetch(`https://rickandmortyapi.com/api/character/?name=${nome}`);
-
         const dados = await resposta.json();
-
         const personagem = dados.results[0];
-
         mostrarCard(personagem);
-
         salvarHistorico(personagem);
-
     } catch {
-
         card.innerHTML = "";
-
         erro.textContent =
             "Personagem não encontrado!";
     }
 }
-
 function mostrarCard(personagem) {
-
     card.innerHTML = `
         <img src="${personagem.image}">
         <h2>${personagem.name}</h2>
-
         <p><strong>Status:</strong>
         ${personagem.status}</p>
-
         <p><strong>Espécie:</strong>
         ${personagem.species}</p>
-
         <p><strong>Gênero:</strong>
         ${personagem.gender}</p>
-
         <p><strong>Origem:</strong>
         ${personagem.origin.name}</p>
     `;
 }
 
 function salvarHistorico(personagem) {
-
     const existe =
         historico.some(item => item.id === personagem.id);
-
     if (existe) return;
-
     historico.push({
         id: personagem.id,
         nome: personagem.name,
         status: personagem.status
     });
-
     localStorage.setItem(
         "historicoRick",
         JSON.stringify(historico)
